33fc4e3f01d0ab48a6f20960d445d14d90751e49
seungwonKim98 <godswan1998@gmail.com>
2021-01-21 18:03:02 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,177 +1,100 @@
 package com.eomcs.pms;
 
-import java.sql.Date;
-import java.util.Scanner;
 
+import com.eomcs.handler.BoardHandler;
+import com.eomcs.handler.BoardHandler2;
+import com.eomcs.handler.BoardHandler3;
+import com.eomcs.handler.BoardHandler4;
+import com.eomcs.handler.BoardHandler5;
+import com.eomcs.handler.BoardHandler6;
+import com.eomcs.handler.MemberHandler;
+import com.eomcs.handler.ProjectHandler;
+import com.eomcs.handler.TaskHandler;
+import com.eomcs.util.Prompt;
 
+// 1) 회원 정보 저장할 때 사용할 메모리의 설계도를 정의한다.
+//    - MemberHandler 클래스 안에 Member 클래스를 정의한다.
+//    - Member 클래스를 사용하여 회원 정보를 다룬다.
+// 2) 프로젝트 정보 저장할 때 사용할 메모리의 설계도를 정의한다.
+//    - ProjectHandler 클래스 안에 Project 클래스를 정의한다.
+//    - Project 클래스를 사용하여 프로젝트 정보를 다룬다.
+// 3) 작업 정보 저장할 때 사용할 메모리의 설계도를 정의한다.
+//    - TaskHandler 클래스 안에 Task 클래스를 정의한다.
+//    - Task 클래스를 사용하여 작업 정보를 다룬다.
+//
 public class App {
-
-  static Scanner keyboardScan = new Scanner(System.in);
-
-
-  //회원 데이터
-  static final int LENGTH = 100;
-  static int[] no = new int[LENGTH];
-  static String[] name = new String[LENGTH];
-  static String[] email = new String[LENGTH];
-  static String[] password = new String[LENGTH];
-  static String[] photo = new String[LENGTH];
-  static String[] tel = new String[LENGTH];
-  static Date[] registeredDate = new Date[LENGTH];
-  static int size = 0;
-
-
-  //프로젝트 데이터
-  static final int PROJECT_LENGTH = 100;
-  static int[] pno = new int[PROJECT_LENGTH];
-  static String[] ptitle = new String[PROJECT_LENGTH];
-  static String[] pcontent = new String[PROJECT_LENGTH];
-  static Date[] pstartDate = new Date[PROJECT_LENGTH];
-  static Date[] pendDate = new Date[PROJECT_LENGTH];
-  static String[] powner = new String[PROJECT_LENGTH];
-  static String[] pmembers = new String[PROJECT_LENGTH];
-  static int psize = 0;
-
-
-  //작업 데이터
-  static final int TASK_LENGTH = 100;
-  static int[] tno = new int[TASK_LENGTH];
-  static String[] tcontent = new String[TASK_LENGTH];
-  static Date[] tdeadline = new Date[TASK_LENGTH];
-  static String[] towner = new String[TASK_LENGTH];
-  static int[] tstatus = new int[TASK_LENGTH];    
-  static int tsize = 0;
-
-  // 문자형 스캐너 메소드
-  static String promptString(String title) {
-    System.out.print(title);
-    return keyboardScan.nextLine();
-  }
-
-  // 인트형 스캐너 메소드
-  static int promptInt(String title) {
-    String str = promptString(title);
-    return Integer.valueOf(str);
-  }
-
-  // 데이트 스캐너 메소드
-  static Date promptDate(String title) {
-    String today = promptString(title);
-    return Date.valueOf(today);
-  }
-
-
-  static void addMember() {
-    System.out.println("[회원 등록]");
-
-    no[size] = promptInt("번호? ");
-    name[size] = promptString("이름? ");
-    email[size] = promptString("이메일? ");
-    password[size] = promptString("암호? ");
-    photo[size] = promptString("사진? ");
-    tel[size] = promptString("전화? ");
-
-    registeredDate[size] = new java.sql.Date(System.currentTimeMillis());
-
-    size++;
-  }
-
-
-  static void listMember() {
-    System.out.println("[회원 목록]");
-
-    for (int i = 0; i < size; i++) {
-      // 번호, 이름, 이메일, 전화, 가입일
-      System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
-          no[i], name[i], email[i], tel[i], registeredDate[i]);
-    }
-  }
-
-  static void addProject() {
-    System.out.println("[프로젝트 등록]");
-
-    pno[psize] = promptInt("번호? ");
-    ptitle[psize] = promptString("프로젝트명? ");
-    pcontent[psize] = promptString("내용? ");
-    pstartDate[psize] = promptDate("시작일? ");
-    pendDate[psize] = promptDate("종료일? ");
-    powner[psize] = promptString("만든이? ");
-    pmembers[psize] = promptString("팀원? ");
-
-    psize++;
-  }
-
-  static void listProject() {
-    System.out.println("[프로젝트 목록]");
-
-    for (int i = 0; i < psize; i++) {
-      // 번호, 프로젝트명, 시작일, 종료일, 만든이
-      System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
-          pno[i], ptitle[i], pstartDate[i], pendDate[i], powner[i]);
-    }
-  }
-
-  static void addTask() {
-    System.out.println("[작업 등록]");
-
-    tno[tsize] = promptInt("번호? ");
-    tcontent[tsize] = promptString("내용? ");
-    tdeadline[tsize] = promptDate("마감일? ");
-    tstatus[tsize] = promptInt("상태?\n0: 신규\n1: 진행중\n2: 완료\n> ");
-    towner[tsize] = promptString("담당자? ");
-
-    tsize++;
-  }
-
-  static void listTask() {
-    System.out.println("[작업 목록]");
-
-
-    for (int i = 0; i < tsize; i++) {
-      String stateLabel = null;
-      switch (tstatus[i]) {
-        case 1:
-          stateLabel = "진행중";
-          break;
-        case 2:
-          stateLabel = "완료";
-          break;
-        default:
-          stateLabel = "신규";
-      }
-      // 번호, 작업명, 마감일, 프로젝트, 상태, 담당자
-      System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
-          tno[i], tcontent[i], tdeadline[i], stateLabel, towner[i]);
-    }
-  }
 
   public static void main(String[] args) {
 
-    while(true){
-      String command = promptString("명령> ");
+    loop:
+      while (true) {
+        String command = com.eomcs.util.Prompt.inputString("명령> ");
 
-      if(command.equalsIgnoreCase("quit") || command.equalsIgnoreCase("exit")) {
-        System.out.println("사용해주셔서 감사합니다.");
-        break;
-
-      }else if(command.equalsIgnoreCase("/member/add")) {
-        addMember();
-      }else if(command.equalsIgnoreCase("/member/list")) {
-        listMember();
-      }else if(command.equalsIgnoreCase("/project/add")){
-        addProject();
-      }else if(command.equalsIgnoreCase("/project/list")) {
-        listProject();
-      }else if(command.equalsIgnoreCase("/task/add")) {
-        addTask();
-      }else if(command.equalsIgnoreCase("/task/list")) {
-        listTask();
-      }else {
-        System.out.println("실행할 수 없는 명령입니다.");
+        switch (command) {
+          case "/member/add":
+            MemberHandler.add();
+            break;
+          case "/member/list":
+            MemberHandler.list();
+            break;
+          case "/project/add":
+            ProjectHandler.add();
+            break;
+          case "/project/list":
+            ProjectHandler.list();
+            break;
+          case "/task/add":
+            TaskHandler.add();
+            break;
+          case "/task/list":
+            TaskHandler.list();
+            break;
+          case "/board/add":
+            BoardHandler.add();
+            break;
+          case "/board/list":
+            BoardHandler.list(); 
+            break;
+          case "/board2/add":
+            BoardHandler2.add();
+            break;
+          case "/board2/list":
+            BoardHandler2.list(); 
+            break;
+          case "/board3/add":
+            BoardHandler3.add();
+            break;
+          case "/board3/list":
+            BoardHandler3.list();
+            break;
+          case "/board4/add":
+            BoardHandler4.add();
+            break;
+          case "/board4/list":
+            BoardHandler4.list();
+            break;
+          case "/board5/add":
+            BoardHandler5.add();
+            break;
+          case "/board5/list":
+            BoardHandler5.list();
+            break;
+          case "/board6/add":
+            BoardHandler6.add();
+            break;
+          case "/board6/list":
+            BoardHandler6.list();
+            break;
+          case "quit":
+          case "exit":
+            System.out.println("안녕!");
+            break loop;
+          default:
+            System.out.println("실행할 수 없는 명령입니다.");
+        }
+        System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
       }
-      System.out.println();
-    }
+
+    Prompt.close();
   }
 }
-
-
--- app/src/test/java/com/eomcs/pms/AppTest.java
+++ app/src/test/java/com/eomcs/pms/AppTest.java
@@ -1,10 +1,10 @@
 package com.eomcs.pms;
 
-import org.junit.Test;
-
 public class AppTest {
-  @Test public void testAppHasAGreeting() {
-    //App classUnderTest = new App();
-    //assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+    System.out.println("반갑습니다.");
   }
+
 }
